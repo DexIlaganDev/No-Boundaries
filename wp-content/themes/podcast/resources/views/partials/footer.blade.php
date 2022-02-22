72e023d3ab72bb11bde1d642cc76fd38725512d3
dexidev <dddexadgat@gmail.com>
2022-02-22 08:49:25 +0800

    open link on a new tab

--- wp-content/themes/podcast/resources/views/partials/footer.blade.php
+++ wp-content/themes/podcast/resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col text-center">
         <section class="text-secondary py-5 border-top border-light">
-          <a class="text-decoration-none text-secondary" href="https://collingsnft.io/">Created by Collings Media</a>
+          <a class="text-decoration-none text-secondary" target="_blank" href="https://collingsnft.io/">Created by Collings Media</a>
         </section>
       </div>
     </div>
